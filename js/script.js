b8abc77c1e92e9bcd31987ef9376e1ad951e022f
DangeloGodoy <dangelogodoy@gmail.com>
2024-07-25 21:20:48 -0400

    update to name for array and vars

--- js/script.js
+++ js/script.js
@@ -1,115 +1,115 @@
 const products = [
     {
         id: 1,
-        nombre: "Smartphone Galaxy S21",
-        marca: "Samsung",
-        tipo: "Smartphone",
-        precio: 799000,
-        imagen: "./img/galaxy_s21.jpg"
+        name: "Smartphone Galaxy S21",
+        marke: "Samsung",
+        type: "Smartphone",
+        price: 799000,
+        img: "./img/galaxy_s21.jpg"
     },
     {
         id: 2,
-        nombre: "iPhone 15 Pro",
-        marca: "Apple",
-        tipo: "Smartphone",
-        precio: 999000,
-        imagen: "./img/iphone_15_pro.jpg"
+        name: "iPhone 15 Pro",
+        marke: "Apple",
+        type: "Smartphone",
+        price: 999000,
+        img: "./img/iphone_15_pro.jpg"
     },
     {
         id: 3,
-        nombre: "Laptop XPS 13",
-        marca: "Dell",
-        tipo: "Laptop",
-        precio: 1199000,
-        imagen: "./img/Laptop_xps_13.jpg"
+        name: "Laptop XPS 13",
+        marke: "Dell",
+        type: "Laptop",
+        price: 1199000,
+        img: "./img/Laptop_xps_13.jpg"
     },
     {
         id: 4,
-        nombre: "MacBook Pro",
-        marca: "Apple",
-        tipo: "Laptop",
-        precio: 1499000,
-        imagen: "./img/macbook_pro.jpg"
+        name: "MacBook Pro",
+        marke: "Apple",
+        type: "Laptop",
+        price: 1499000,
+        img: "./img/macbook_pro.jpg"
     },
     {
         id: 5,
-        nombre: "Televisor OLED CX",
-        marca: "LG",
-        tipo: "Televisor",
-        precio: 1299000,
-        imagen: "./img/lg_oled_cx.jpg"
+        name: "Televisor OLED CX",
+        marke: "LG",
+        type: "Televisor",
+        price: 1299000,
+        img: "./img/lg_oled_cx.jpg"
     },
     {
         id: 6,
-        nombre: "Televisor QLED Q60T",
-        marca: "Samsung",
-        tipo: "Televisor",
-        precio: 1099000,
-        imagen: "./img/samsung_qled_q60t.jpg"
+        name: "Televisor QLED Q60T",
+        marke: "Samsung",
+        type: "Televisor",
+        price: 1099000,
+        img: "./img/samsung_qled_q60t.jpg"
     },
     {
         id: 7,
-        nombre: "Tablet Galaxy Tab S7",
-        marca: "Samsung",
-        tipo: "Tablet",
-        precio: 649000,
-        imagen: "./img/galaxy_tab_s7.jpg"
+        name: "Tablet Galaxy Tab S7",
+        marke: "Samsung",
+        type: "Tablet",
+        price: 649000,
+        img: "./img/galaxy_tab_s7.jpg"
     },
     {
         id: 8,
-        nombre: "iPad Pro",
-        marca: "Apple",
-        tipo: "Tablet",
-        precio: 899000,
-        imagen: "./img/ipad_pro.jpg"
+        name: "iPad Pro",
+        marke: "Apple",
+        type: "Tablet",
+        price: 899000,
+        img: "./img/ipad_pro.jpg"
     },
     {
         id: 9,
-        nombre: "Auriculares WH-1000XM4",
-        marca: "Sony",
-        tipo: "Auriculares",
-        precio: 349000,
-        imagen: "./img/sony_wh1000xm4.jpg"
+        name: "Auriculares WH-1000XM4",
+        marke: "Sony",
+        type: "Auriculares",
+        price: 349000,
+        img: "./img/sony_wh1000xm4.jpg"
     },
     {
         id: 10,
-        nombre: "Auriculares AirPods Pro",
-        marca: "Apple",
-        tipo: "Auriculares",
-        precio: 249000,
-        imagen: "./img/airpods_pro.jpg"
+        name: "Auriculares AirPods Pro",
+        marke: "Apple",
+        type: "Auriculares",
+        price: 249000,
+        img: "./img/airpods_pro.jpg"
     }
 ];
 
 function cardProducts() {
-    const contenedor = document.getElementById('listProducts');
+    const contaniner = document.getElementById('listProducts');
 
     products.forEach(product => {
         let productDiv = document.createElement("div");
         productDiv.className = "card mb-4 style-card";
 
         let image = document.createElement("img");
-        image.src = product.imagen;
-        image.alt = product.nombre;
+        image.src = product.img;
+        image.alt = product.name;
         image.className = "card-img-top img-thumbnail img-class";
 
         let bodyDiv = document.createElement("div");
         bodyDiv.className = "card-body";
 
         let name = document.createElement("h5");
-        name.textContent = product.nombre;
+        name.textContent = product.name;
         name.className = "card-title text-wrap";
 
         let company = document.createElement("p");
-        company.textContent = product.marca;
+        company.textContent = product.marke;
         company.className = "card-subtitle"
 
         let type = document.createElement("p");
         type.className = "card-text";
-        type.textContent = product.tipo;
+        type.textContent = product.type;
 
         let price = document.createElement("p");
-        price.textContent = `$ ${product.precio}`;
+        price.textContent = `$ ${product.price}`;
         price.className = "card-text";
 
         let button = document.createElement("button");
@@ -128,7 +128,7 @@ function cardProducts() {
         productDiv.appendChild(image);
         productDiv.appendChild(bodyDiv);
 
-        contenedor.appendChild(productDiv);
+        contaniner.appendChild(productDiv);
     });
 }
 
@@ -136,9 +136,9 @@ function addToCart(product) {
     let productsCart = JSON.parse(localStorage.getItem('carrito')) || [];
     let foundproducts = productsCart.find(item => item.id === product.id);
     if (foundproducts) {
-        foundproducts.cantidad++;
+        foundproducts.count++;
     } else {
-        product.cantidad = 1;
+        product.count = 1;
         productsCart.push(product);
     }
     localStorage.setItem('carrito',JSON.stringify(productsCart));
@@ -149,8 +149,8 @@ function removeCart(id) {
     let cart = JSON.parse(localStorage.getItem('carrito')) || [];
     let index = cart.findIndex(item => item.id === id);
     if (index !== -1) {
-        cart[index].cantidad--;
-        if (cart[index].cantidad === 0) {
+        cart[index].count--;
+        if (cart[index].count === 0) {
             cart.splice(index, 1);
         }
         localStorage.setItem('carrito', JSON.stringify(cart));
@@ -176,15 +176,15 @@ function cartProducts() {
 
         let name = document.createElement("h5");
         name.className = "cart-item-title";
-        name.textContent = product.nombre;
+        name.textContent = product.name;
 
         let cant = document.createElement("p")
         cant.className = "cart-item-price"
-        cant.textContent = `Cantidad de producto: ${product.cantidad}`
+        cant.textContent = `Cantidad de productos: ${product.count}`
 
         let price = document.createElement("p");
         price.className = "cart-item-price";
-        price.textContent = `$ ${product.precio * product.cantidad}`;
+        price.textContent = `$ ${product.price * product.count}`;
 
         let removeProduct = document.createElement("button");
         removeProduct.className = "btn btn-outline-danger";
@@ -197,7 +197,7 @@ function cartProducts() {
         productDiv.appendChild(removeProduct);
 
         containerCart.appendChild(productDiv);
-        total += product.precio * product.cantidad
+        total += product.precio * product.count
     });
 
     let totalCart = document.getElementById('totalCart');
