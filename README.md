fe50443fefa825c49610f6f0016b7d69bf348a9f
Dangelo Godoy <dangelogodoy@gmail.com>
2024-08-16 10:28:24 -0400

    add secction for card, cvc and date for card pay, and logic the validation for card with data in the readme file

--- README.md
+++ README.md
@@ -7,3 +7,20 @@
   <li><b>desc25</b>: Aplica un 25% de descuento en el total</li>
   <li><b>desc50</b>: Aplica un 50% de descuento en el total</li>
 </ul>
+
+<h3>Datos para probar el pago</h3>
+<p>Se dejan datos de tarjeta para realizar las pruebas de los pagos</p>
+<ul>
+  <p>Cliente 1</p>
+  <ul>
+    <li><b>Número Tarjeta:</b>4548865105665084</li>
+    <li><b>CVC:</b>180</li>
+    <li><b>Expiración:</b>06/27</li>
+  </ul>
+  <p>Cliente 2</p>
+  <ul>
+    <li><b>Número Tarjeta:</b>4220382712576402</li>
+    <li><b>CVC:</b>997</li>
+    <li><b>Expiración:</b>10/27</li>
+  </ul>
+</ul>
--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,13 @@
 function removeCart(id) {
-    let cart = JSON.parse(localStorage.getItem('carrito')) || [];
-    let index = cart.findIndex(item => item.id === id);
+    let cart = JSON.parse(localStorage.getItem('carrito')) || []
+    let index = cart.findIndex(item => item.id === id)
     if (index !== -1) {
-        cart[index].count--;
+        cart[index].count--
         if (cart[index].count === 0) {
-            cart.splice(index, 1);
+            cart.splice(index, 1)
         }
-        localStorage.setItem('carrito', JSON.stringify(cart));
-        cartProducts();
+        localStorage.setItem('carrito', JSON.stringify(cart))
+        cartProducts()
     }
 }
 
@@ -35,7 +35,7 @@ function cartProducts() {
             cant.className = "cart-item-price"
             cant.textContent = `Cantidad de productos: ${product.count}`
 
-            subTotal = product.price * product.count;
+            subTotal = product.price * product.count
             let price = document.createElement("p")
             price.className = "cart-item-price"
             price.textContent = `$ ${subTotal.toLocaleString('es-CL')}`
@@ -54,7 +54,7 @@ function cartProducts() {
                         y: 30
                     },
 
-                }).showToast();
+                }).showToast()
             }
 
             productDiv.appendChild(name)
@@ -63,7 +63,7 @@ function cartProducts() {
             productDiv.appendChild(removeProduct)
 
             containerCart.appendChild(productDiv)
-            total += subTotal;
+            total += subTotal
         })
     }
 
@@ -91,13 +91,13 @@ function cartProducts() {
                             title: "Descuento Aplicado",
                             html: `Se aplico correctamente el codigo <b>${foundCode.code}</b> con un ${foundCode.discount}% de descuento`,
                             icon: "success"
-                        });
+                        })
                     } else {
                         Swal.fire({
                             title: "Codigo invalido o vacio",
                             text: "El codigo ingresado no es valido o el campo esta vacio",
                             icon: "error"
-                        });
+                        })
                     }
 
                     let totalCart = document.getElementById('totalCart')
@@ -114,31 +114,70 @@ function processPay() {
     const paymentForm = document.getElementById('paymentForm')
     const backCartButton = document.getElementById('backCart')
     const cartDisplay = document.getElementById('cart')
+    const numberCard = document.getElementById('numberCard')
+    const numberCvc = document.getElementById('cvc')
+    const dateTime = document.getElementById('dateTime')
+    const numberZip = document.getElementById('zipCode')
 
     paymentForm.style.display = 'none'
     backCartButton.classList.add('disabled')
 
-    // Muestra el formulario de pago y oculta el carrito al hacer clic en 'Ir a Pagar'
+    // Muestra el formulario de pago y oculta el carrito al hacer clic en 'Ir a Pagar', además se desabilita el 
     document.getElementById('payCart').onclick = () => {
-        cartDisplay.style.display = 'none';
-        paymentForm.style.display = 'block';
+        cartDisplay.style.display = 'none'
+        paymentForm.style.display = 'block'
         backCartButton.classList.remove('disabled')
-    };
+        let buttonPay = document.getElementById('payCart')
+        buttonPay.classList.add('disabled')
+    }
     (() => {
-        const form = document.getElementById('checkoutForm');
-        form.addEventListener('submit', validateSendForm, false);
-    })();
+        const form = document.getElementById('checkoutForm')
+        form.addEventListener('submit', validateSendForm, false)
+    })()
+
+    dateTime.addEventListener('input', function (e) {
+        let value = e.target.value.replace(/\D/g, '') // se limita solo a numero y la cantidad de caracteres
+        if (value.length >= 2) {
+            value = value.slice(0, 2) + '/' + value.slice(2, 4)
+        }
+        e.target.value = value.slice(0, 5)
+    })
+
+    numberZip.addEventListener('input', function (e) {
+        e.target.value = e.target.value.replace(/\D/g, '')
+        if (e.target.value.length > 7) {
+            e.target.value = e.target.value.slice(0, 7)
+        }
+    })
+
+    numberCvc.addEventListener('input', function (e) {
+        e.target.value = e.target.value.replace(/\D/g, '')
+        if (e.target.value.length > 3) {
+            e.target.value = e.target.value.slice(0, 3)
+        }
+    })
+
+    numberCard.addEventListener('input', function (e) {
+        let value = e.target.value.replace(/\D/g, '')
+        if (value.length >= 4) {
+            value = value.slice(0, 4) + ' ' + value.slice(4, 8) + ' ' + value.slice(8, 12) + ' ' + value.slice(12, 16)
+        }
+        e.target.value = value.slice(0, 19)
+    })
 }
 
 // Manejo para el envío del formulario
 
 function validateSendForm(event) {
+    'use strict'
+    const numberCard = document.getElementById('numberCard')
+    const numberCvc = document.getElementById('cvc')
+    const dateTime = document.getElementById('dateTime')
     const pageURL = window.location.origin
     try {
-        'use strict'
-        event.preventDefault() // Evitamos el envio del formulario
+        event.preventDefault() // Evitamos el envío del formulario
         const form = document.getElementById('checkoutForm')
-
+        
         if (!form.checkValidity()) {
             event.stopPropagation()
             Swal.fire({
@@ -155,29 +194,45 @@ function validateSendForm(event) {
                 html: "Nos encontramos validando tu pago...",
                 timer: 5000,
                 timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading();
+                },
             }).then((result) => {
                 if (result.dismiss === Swal.DismissReason.timer) {
-                    Swal.fire({
-                        title: 'Pago Realizado',
-                        text: 'Se realizo el pago correctamente, sera enviado a la pagina de inicio',
-                        icon: 'success',
-                        confirmButtonText: 'Aceptar'
-                    })
-                    setTimeout(() => {
-                        localStorage.removeItem('total')
-                        localStorage.removeItem('carrito')
-                        form.submit()
-                        window.location.href = pageURL
-                    },5000)
-                } else {
-                    throw new Error("No se logro procesar el pago correctamente!!")
+                    let cardNumber = numberCard.value.replace(/\D/g, '')
+                    let date = dateTime.value.replace(/\D/g, '')
+                    let cvc = numberCvc.value
+                    if ((cardNumber === '4548865105665084' && date === '0627' && cvc === '180') || (cardNumber === '4220382712576402' && date === '1027' && cvc === '997')) {
+                        Swal.fire({
+                            title: 'Pago Realizado',
+                            text: 'Se realizó el pago correctamente, será enviado a la página de inicio',
+                            icon: 'success',
+                            confirmButtonText: 'Aceptar'
+                        })
+
+                        setTimeout(() => {
+                            localStorage.removeItem('total')
+                            localStorage.removeItem('carrito')
+                            form.submit()
+                            window.location.href = pageURL
+                        }, 5000)
+                    } else {
+                        Swal.fire({
+                            title: 'Error en el Pago',
+                            text: 'No se logró procesar el pago correctamente',
+                            icon: 'error',
+                            confirmButtonText: 'Aceptar'
+                        })
+                        throw new Error("Ocupar los datos del Readme por favor :D")
+                    }
                 }
             })
         }
     } catch (err) {
-        console.log('Se dectecto el siguiente error al enviar el formulario: ', err)
+        console.error('Se detectó el siguiente error al enviar el formulario: ', err)
     }
 }
+
 
 function listState() {
     // Cargar comunas desde un archivo JSON y rellenar el select
